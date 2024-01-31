eecc18c31010c674937422a577ff1cb9b18bc0d0
Daniel Souza <149512463+dev-danielsouza@users.noreply.github.com>
2024-01-31 13:14:33 -0300

    Create script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('That`s All Folks')
+
+var nome = 'Daniel Souza'
+var age = 19
+var sexo = 'M'
+var job = 'progamming student'
+
+console.log(`Nome de usuário: ${nome}`)
+console.log(`Idade: ${age}`)
+console.log(`Sexo: ${sexo}`)
+console.log(`Job: ${job}`)
