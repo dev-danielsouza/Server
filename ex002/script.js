25ae049d499d5e02c51230f5f267c939bc5b3bb3
Daniel Souza <149512463+dev-danielsouza@users.noreply.github.com>
2024-02-01 11:41:59 -0300

    Create ex002

---
+++ ex002/script.js
@@ -0,0 +1,9 @@
+var maças = 3
+var todasVerdes = false
+
+if (maças > 1 && todasVerdes === true) {
+    console.log('Temos mais de uma maça e todas as maças são verdes')
+}
+else {
+    console.log('Não temos mais de uma maça ou nem todas são verdes')
+}
